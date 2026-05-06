1c1ed2ddc52eb0b1e8e5f9bfaea198d889b7924d
Artsiom Herasimchyk <artem.ice33@gmail.com>
2026-05-06 12:34:55 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
-module.exports = function check(/* str, bracketsConfig */) {
-  throw new Error('Not implemented');
+module.exports = function check(str, bracketsConfig) {
+  const pairs = bracketsConfig.map((pair) => pair.join(''));
+  let current = str;
+  let lastLength = -1;
+
+  while (current.length !== lastLength) {
+    lastLength = current.length;
+    for (let i = 0; i < pairs.length; i += 1) {
+      current = current.replace(pairs[i], '');
+    }
+  }
+
+  return current.length === 0;
 };
